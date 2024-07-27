9c984036ed27b5c1a99547726fe1f78dbeb6125e
eblanyrT <mathiusibrahim@gmail.com>
2024-07-27 11:22:19 +0700

    update the instruction after done installing the script

--- install.sh
+++ install.sh
@@ -97,4 +97,4 @@ sudo cp .config/i3/config ~/.config/i3
 echo " "
 
 echo "Installation Complete!"
-echo "Follow the instructions on README.md to apply theme on Rofi, GTK, Icons, etc.."
+echo "Follow the further instructions on README.md to apply themes..."
